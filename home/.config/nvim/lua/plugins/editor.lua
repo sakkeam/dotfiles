fbf03ff2abcc42ed8ff8770d034f6e33252dc829
Konosuke Sakai <konosukeam@gmail.com>
2025-09-08 21:26:35 +0900

    feat(editor.lua): add lazygit integration with toggleterm for improved terminal management in Neovim

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -2,6 +2,27 @@ return {
 	{
 		"akinsho/toggleterm.nvim",
 		version = "*",
-		config = true,
+		config = function()
+			require("toggleterm").setup({})
+
+			local Terminal = require("toggleterm.terminal").Terminal
+
+			local lazygit = Terminal:new({
+				cmd = "lazygit",
+				direction = "float",
+				hidden = true,
+			})
+
+			function _G._lazygit_toggle()
+				lazygit:toggle()
+			end
+
+			vim.api.nvim_set_keymap(
+				"n",
+				"<Leader>g",
+				"<Cmd>lua _lazygit_toggle()<CR>",
+				{ silent = true, noremap = true }
+			)
+		end,
 	},
 }
