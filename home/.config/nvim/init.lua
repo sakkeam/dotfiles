920fd197595d9893692d0face6d08a9fd937057f
Konosuke Sakai <konosukeam@gmail.com>
2025-09-09 00:34:03 +0900

    style(nvim): move tabstop setting below shiftwidth for better organization

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -3,8 +3,8 @@ require("config.lazy")
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
 vim.g.mapleader = " "
 
