2dac11123fe9a81c431d4bdfcaf199a3816cee35
Konosuke Sakai <konosukeam@gmail.com>
2025-09-08 20:49:13 +0900

    feat(lsp.lua): add autocmd for formatting on buffer write to enhance code quality and maintain consistency

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -36,6 +36,13 @@ return {
 					lua = { "stylua" },
 				},
 			})
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
 		end,
 	},
 }
