720cf4c93a8678682109c77264fa1d058e39281f
Konosuke Sakai <konosukeam@gmail.com>
2025-09-08 20:48:11 +0900

    feat(lsp.lua): add conform.nvim plugin for formatting Lua files with stylua to enhance code quality and consistency

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -28,4 +28,14 @@ return {
     "folke/trouble.nvim",
     opts = {},
   },
+  {
+    "stevearc/conform.nvim",
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          lua = { "stylua" },
+        },
+      })
+    end,
+  },
 }
