0a562b59aeecd0f077bc1b2254d0ff6f9bad8dc7
Konosuke Sakai <konosukeam@gmail.com>
2025-09-08 20:49:05 +0900

    style(lsp.lua): format code for better readability and consistency in indentation

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,41 +1,41 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-  },
-  {
-    "neovim/nvim-lspconfig",
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        -- lua
-        "lua_ls",
-        "stylua",
-      },
-    },
-  },
-  {
-    "folke/trouble.nvim",
-    opts = {},
-  },
-  {
-    "stevearc/conform.nvim",
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          lua = { "stylua" },
-        },
-      })
-    end,
-  },
+	{
+		"mason-org/mason.nvim",
+		opts = {},
+	},
+	{
+		"neovim/nvim-lspconfig",
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {},
+		dependencies = {
+			"mason-org/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
+			ensure_installed = {
+				-- lua
+				"lua_ls",
+				"stylua",
+			},
+		},
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {},
+	},
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+				},
+			})
+		end,
+	},
 }
