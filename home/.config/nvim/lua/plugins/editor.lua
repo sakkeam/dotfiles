fcac56cb950d8a3d9451d63f3ec55d244a0b22b8
Konosuke Sakai <konosukeam@gmail.com>
2025-09-09 02:14:50 +0900

    feat(editor.lua): add nvim-tree plugin configuration for file navigation in Neovim to enhance user experience

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -247,4 +247,13 @@ return {
 			"nvim-lua/plenary.nvim",
 		},
 	},
+	{
+		"nvim-tree/nvim-tree.lua",
+		config = function()
+			vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
+
+			require("nvim-tree").setup()
+		end,
+	},
 }
