992719933fda023b28998348dee3e862eb185308
Konosuke Sakai <konosukeam@gmail.com>
2025-09-09 13:56:09 +0900

    feat(editor.lua): add configuration for indent-blankline.nvim to support rainbow indentation colors
    This change enhances the visual representation of indentation levels in Neovim by implementing rainbow colors for better code readability.

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -280,7 +280,34 @@ return {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
-		opts = {},
+		config = function()
+			local highlight = {
+				"RainbowRed",
+				"RainbowYellow",
+				"RainbowBlue",
+				"RainbowOrange",
+				"RainbowGreen",
+				"RainbowViolet",
+				"RainbowCyan",
+			}
+
+			local hooks = require("ibl.hooks")
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#F38BA8" })
+				vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#F9E2AF" })
+				vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#89B4FA" })
+				vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#FAB387" })
+				vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#A6E3A1" })
+				vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#CBA6F7" })
+				vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#94E2D5" })
+			end)
+
+			require("ibl").setup({
+				indent = {
+					highlight = highlight,
+				},
+			})
+		end,
 	},
 	{
 		"brenoprata10/nvim-highlight-colors",
