fdaa5618e172a9a238f6d063e8eface17fc590c8
Konosuke Sakai <konosukeam@gmail.com>
2025-09-09 16:23:12 +0900

    feat(editor.lua): enhance lualine configuration by adding mode section to display current mode in status line

--- home/.config/nvim/lua/plugins/editor.lua
+++ home/.config/nvim/lua/plugins/editor.lua
@@ -65,12 +65,21 @@ return {
 	},
 	{
 		"nvim-lualine/lualine.nvim",
-		opts = {
-			options = {
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-			},
-		},
+		config = function()
+			local function mode()
+				return vim.fn.mode()
+			end
+
+			require("lualine").setup({
+				options = {
+					component_separators = { left = "", right = "" },
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = { mode },
+				},
+			})
+		end,
 		dependencies = {
 			"nvim-tree/nvim-web-devicons",
 		},
