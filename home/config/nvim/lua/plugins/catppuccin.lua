9b1f1c3cd5ad0b76bcfca4594244e9cef244236f
Konosuke Sakai <konosukeam@gmail.com>
2025-09-16 19:52:05 +0900

    style(catppuccin.lua): format code to use consistent indentation with tabs for better readability

--- home/config/nvim/lua/plugins/catppuccin.lua
+++ home/config/nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,12 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    require("catppuccin").setup({
-      transparent_background = true,
-    })
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
 
-    vim.cmd.colorscheme "catppuccin"
-  end,
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
